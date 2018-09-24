1f355932e7da61956e53e4ffa2eb1328a1153f1b
Tye Borden <tyeborden@gmail.com>
2018-09-23 21:49:47 -0300

    Adding html, JS, and other files, need to be eidited

---
+++ SoftEng/scripts/Navigation.js
@@ -0,0 +1,80 @@
+var SERVER_URL = 'http://140.184.132.239:3000';
+
+if (!sessionStorage) {
+  alert(
+    'Warning: Your browser does not support features required for this application, please consider upgrading.'
+  );
+}
+
+/* Adds given text value to the password text 
+ * field
+ */
+function addValueToPassword(button) {
+  var currVal = $("#passcode").val();
+  if (button == "bksp") {
+    $("#passcode").val(currVal.substring(0,
+      currVal.length - 1));
+  } else {
+    $("#passcode").val(currVal.concat(button));
+  }
+}
+
+/* On the main page, after password entry, directs
+ * user to main page, legal disclaimer if they
+ * have not yet agreed to it
+ */
+$("#btnEnter").click(function () {
+  var loginCredentials = {
+    email: $("#email").val(),
+    password: $("#passcode").val()
+  };
+  $.post(SERVER_URL + '/login',
+    loginCredentials,
+    function (data) {
+      if (data && data.email ==
+        loginCredentials.email) {
+        sessionStorage.password = $(
+          "#passcode").val();
+        sessionStorage.user = JSON.stringify(
+          data);
+        if (!data.agreedToLegal) {
+          return $.mobile.changePage(
+            "#legalNotice");
+        }
+        $.post(SERVER_URL + '/getRecords',
+          loginCredentials,
+          function (data) {
+            sessionStorage.tbRecords =
+              JSON.stringify(data);
+            $.mobile.changePage(
+              "#pageMenu");
+          }).fail(function (error) {
+          alert(error.responseText);
+        })
+      } else {
+        alert(
+          'An error occurred logging user in.'
+        );
+      }
+    }).fail(function (error) {
+    alert(error.responseText);
+  });
+});
+
+/* Records that the user has agreed to the legal
+ * disclaimer on this device/browser
+ */
+$("#noticeYes").click(function () {
+  var user = JSON.parse(sessionStorage.user);
+  user.agreedToLegal = 1; // True
+  user.password = sessionStorage.password;
+  $.post(SERVER_URL + '/updateUser', user,
+    function (data) {
+      sessionStorage.user = JSON.stringify(
+        user);
+      return $.mobile.changePage(
+        "#pageMenu");
+    }).fail(function (error) {
+    alert(error.responseText);
+  });
+});
---
+++ SoftEng/scripts/UserForm.js
@@ -0,0 +1,144 @@
+var SERVER_URL = 'http://127.0.0.1:3000';
+
+$("#btnUserClear").click(function () {
+  clearUserForm();
+});
+
+$("#btnUserUpdate").click(function () { //Event : submitting the form
+  saveUserForm();
+  return true;
+});
+
+function checkUserForm() { //Check for empty fields in the form
+  //for finding current date 
+  var d = new Date();
+  var month = d.getMonth() + 1;
+  var date = d.getDate();
+  var year = d.getFullYear();
+  var currentDate = year + '/' +
+    (('' + month).length < 2 ? '0' : '') +
+    month + '/' +
+    (('' + date).length < 2 ? '0' : '') + date;
+
+  if (($("#txtEmail").val() != "") &&
+    ($("#txtFirstName").val() != "") &&
+    ($("#txtLastName").val() != "") &&
+    ($("#txtHealthCardNumber").val() != "") &&
+    ($("#datBirthdate").val() != "") && ($(
+      "#datBirthdate").val() <= currentDate) &&
+    ($("#slcCancerType option:selected").val() !=
+      "Select Cancer Type") &&
+    ($("#slcCancerStage option:selected").val() !=
+      "Select Cancer Stage") &&
+    ($("#slcTSHRange option:selected").val() !=
+      "Select TSH Range")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function saveUserForm() {
+  if (checkUserForm()) {
+    var user = {
+      "email": $("#txtEmail").val(),
+      "firstName": $("#txtFirstName").val(),
+      "lastName": $("#txtLastName").val(),
+      "healthCardNumber": $(
+        "#txtHealthCardNumber").val(),
+      "newPassword": $("#changePassword").val(),
+      "dateOfBirth": $("#datBirthdate").val(),
+      "cancerType": $(
+        "#slcCancerType option:selected").val(),
+      "cancerStage": $(
+        "#slcCancerStage option:selected").val(),
+      "tshRange": $(
+        "#slcTSHRange option:selected").val()
+    };
+    if ($("#btnUserUpdate").val() == "Create") {
+      var userData = {
+        newUser: user
+      }
+      $.post(SERVER_URL + '/saveNewUser',
+        userData,
+        function (data) {
+          alert(
+            "New User Created Successfully!"
+          );
+          sessionStorage.user = JSON.stringify(
+            user);
+          sessionStorage.password = user.newPassword;
+          $("#btnUserUpdate").val("Update");
+          $.mobile.changePage("#pageMenu");
+          window.location.reload();
+        }).fail(function (error) {
+        alert(error.responseText);
+      });
+    } else {
+      user.agreedToLegal = JSON.parse(
+        sessionStorage.user).agreedToLegal;
+      user.password = sessionStorage.password;
+      $.post(SERVER_URL + '/updateUser', user,
+        function (data) {
+          sessionStorage.user = JSON.stringify(
+            user);
+          sessionStorage.password = user.newPassword;
+        }).fail(function (error) {
+        alert(error.responseText);
+      }).done(function () {
+        $.mobile.changePage("#pageMenu");
+        window.location.reload();
+      });
+    }
+  } else {
+    alert("Please complete the form properly.");
+  }
+}
+
+function clearUserForm() {
+  sessionStorage.clear("user");
+  alert("The stored data have been removed");
+  $("#slcCancerStage").val(
+    "Select Cancer Stage");
+  $('#slcCancerStage').selectmenu('refresh',
+    true);
+  $("#slcCancerType").val("Select Cancer Type");
+  $('#slcCancerType').selectmenu('refresh',
+    true);
+  $("#slcTSHRange").val("Select TSH Range");
+  $('#slcTSHRange').selectmenu('refresh', true);
+}
+
+function showUserForm() { //Load the stored values in the form
+  if (sessionStorage.user != null) {
+    $("#btnUserUpdate").val("Update").button(
+      "refresh");
+    var user = JSON.parse(sessionStorage.user);
+    $("#txtEmail").val(user.email);
+    $("#txtFirstName").val(user.firstName);
+    $("#txtLastName").val(user.lastName);
+    $("#txtHealthCardNumber").val(user.healthCardNumber);
+    $("#changePassword").val(user.newPassword);
+    $("#datBirthdate").val(user.dateOfBirth);
+    $('#slcCancerType option[value=' + user.cancerType +
+      ']').attr('selected', 'selected');
+    $("#slcCancerType option:selected").val(
+      user.cancerType);
+    $('#slcCancerType').selectmenu('refresh',
+      true);
+    $('#slcCancerStage option[value=' + user.cancerStage +
+      ']').attr('selected', 'selected');
+    $("#slcCancerStage option:selected").val(
+      user.cancerStage);
+    $('#slcCancerStage').selectmenu('refresh',
+      true);
+    $('#slcTSHRange option[value=' + user.tshRange +
+      ']').attr('selected', 'selected');
+    $("#slcTSHRange option:selected").val(user.tshRange);
+    $('#slcTSHRange').selectmenu('refresh',
+      true);
+  } else {
+    $("#btnUserUpdate").val("Create").button(
+      "refresh");
+  }
+}
